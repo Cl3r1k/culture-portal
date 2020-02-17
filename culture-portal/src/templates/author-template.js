20354a572f87eee1ac07f67ead4428c556270305
Igor Musmen <musmen@tut.by>
2020-02-17 22:12:09 +0300

    feat: add base author-page elements, implement unstructured data loading

--- culture-portal/src/templates/author-template.js
+++ culture-portal/src/templates/author-template.js
@@ -1,30 +1,37 @@
-import React from "react"
+import React from 'react';
 
-export default ({ pageContext: { author } }) => (
-  <section>
-    <div className="author-page">
-      <h1>
-        {author.name} {author.surname}
-      </h1>
-      <div>Years of life</div>
-      <div>Her/his picture</div>
-      <div>Biography in the form of timeline (example here)</div>
-      <div>List of artist's works with the date of creation</div>
-      <div>
-        Photo gallery with author's picture and pictures of his/her works
+export default ({ pageContext: { author } }) => {
+  const { fullName, photo, description, birth, death, biography, works } = author;
+
+  return (
+    <section>
+      <div className='author-page'>
+        <h1>
+          {fullName}
+        </h1>
+        <p>{birth.date} - {death.date}</p>
+        <div>
+          <img src={photo.link} alt={`${fullName}. Фотография`}/>
+        </div>
+        <p>{description}</p>
+        {/* <TimeLine dataList={biography}/> */}
+        {/* <AuthorWorkList  dataList={works}> */}
+        <div>
+          Photo gallery with author's picture and pictures of his/her works
+        </div>
+        <div>
+          Youtube video about the author / works / period of time author lived.
+          Video must open in a new overlay (modal)
+        </div>
+        <div>
+          Place of author's major activity on a map: google / openstreetmap
+          (leaflet) / yandex
+        </div>
+        <div>
+          It's okay if not every element (timeline, video, photo gallery, map)
+          will be present on every page
+        </div>
       </div>
-      <div>
-        Youtube video about the author / works / period of time author lived.
-        Video must open in a new overlay (modal)
-      </div>
-      <div>
-        Place of author's major activity on a map: google / openstreetmap
-        (leaflet) / yandex
-      </div>
-      <div>
-        It's okay if not every element (timeline, video, photo gallery, map)
-        will be present on every page
-      </div>
-    </div>
-  </section>
-)
+    </section>
+  );
+}
