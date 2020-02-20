458db6bef2206c2de4a71ac1c297cbd1c11d7625
Igor Musmen <musmen@tut.by>
2020-02-21 00:42:32 +0300

    feat: add portal description and author of the day

--- culture-portal/src/helpers/Constants.js
+++ culture-portal/src/helpers/Constants.js
@@ -18,3 +18,21 @@ export const AUTHOR_WORKS = {
     SECOND_COLUMN: "Название произведения",
   },
 }
+
+export const PORTAL_DESCRIPTION = {
+  ru: [
+    "Наш портал посвящен писателям Республики Беларусь. В целом идея таска хороша тем, что в большинстве своем разработчики имеют технический склад ума и соответствующее образование, окружение. В современном ритме жизни не всегда удается уделить достаточно времени вопросам культуры и искусства, творчества, поэзии. А зачастую эти гуманитарные сферы позволяют найти ответы на некоторые жизненные вопросы, по другому взглянуть на мир, найти для себя что-то новое, пересмотреть ценности...",
+    "Интересно отметить и то, что один из разработчиков нашей команды из Новосибирска, где  в отличие от Беларуси этой зимой был снег, но так близко познакомиться с творчеством и биографией Купалы, Бородулина, Панченко появилась возможность фактически благодаря таким непоэтичным словам как RSSchool, React, Gatsby, GraphQL, JavaScript, Git... )",
+    "Мы предлагаем и Вам окунуться в мир прекрасной белорусской поэзии"
+  ],
+  by: [
+    "Наш партал прысвечаны пісьменнікам Рэспублікі Беларусь. У цэлым ідэя задачы добрая тым, што ў большасці сваёй распрацоўшчыкі маюць тэхнічны склад розуму і адпаведную адукацыю, асяроддзе. У сучасным рытме жыцця не заўсёды атрымоўваецца надаць дастаткова часу пытанням культуры і мастацтва, творчасці, паэзіі. А часцяком гэтыя гуманітарныя сферы дазваляюць знайсці адказы на некаторыя жыццёвыя пытанні, па іншаму зірнуць на свет, знайсці для сябе нешта новае, перагледзець каштоўнасці...",
+    "Цікава адзначыць і тое, што адзін з распрацоўшчыкаў нашай каманды з Новасібірска, дзе ў адрозненне ад Беларусі гэтай зімой быў снег, але так блізка пазнаёміцца з творчасцю і біяграфіяй Купалы, Барадуліна, Панчанка з'явілася магчымасць фактычна дзякуючы такім непаятычным словам як RSSchool, React, Gatsby, GraphQL, JavaScript, Git ...)",
+    "Мы прапануем і Вам акунуцца ў свет выдатнай беларускай паэзіі"
+  ],
+  en: [
+    "Our portal is dedicated to writers of the Republic of Belarus. In general, the task idea is good in that for the most part developers have a technical mentality and an appropriate education, environment. In the modern rhythm of life, it is not always possible to devote enough time to issues of culture and art, creativity, poetry. And often, these humanitarian spheres make it possible to find answers to some vital questions, take a different look at the world, find something new for oneself, revise values...",
+    "It is also interesting to note that one of the developers of our team is from Novosibirsk, where it was snowing this winter unlike in Belarus, but the opportunity to get acquainted so close  with the work and biography of Kupala, Borodulin, Panchenko appeared actually thanks to such prosaic words as RSSchool, React, Gatsby, GraphQL, JavaScript, Git ...)",
+    "We invite you to plunge into the beautiful world Belarusian poetry"
+  ],
+}
