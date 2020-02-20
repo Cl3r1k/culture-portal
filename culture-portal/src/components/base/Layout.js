8ea5c908a0f8fe539690d35401a0fa1b4239eef6
Maria_Veremey <Maria_Veremey@epam.com>
2020-02-20 18:26:56 +0300

    fit: add material ui

--- culture-portal/src/components/base/Layout.js
+++ culture-portal/src/components/base/Layout.js
@@ -1,9 +1,33 @@
 import React from "react"
+import  CssBaseline  from '@material-ui/core/CssBaseline';
+import  Container  from '@material-ui/core/Container';
+import  Box  from '@material-ui/core/Box';
 import Navigation from "./Navigation"
+import Language from "../common/Language"
+import "../../scss/styles.scss"
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
-export default ({ children }) => (
+export default ({children}) => {
+
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  return (
   <>
-    <Navigation />
-    {children}
+    <MuiThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Container maxWidth="lg" className="wrapper">
+      <Box className="top-header">
+        <a href="/" className="logo"></a>
+        <Language/>
+      </Box>
+      <Navigation/>
+      {children}
+    </Container>
+    </MuiThemeProvider>
   </>
-)
+  )
+}
--- culture-portal/src/pages/styleguide.js
+++ culture-portal/src/pages/styleguide.js
@@ -1,12 +1,27 @@
 import React from "react"
 import Layout from "../components/base/Layout"
+import Button from "../components/common/Button"
+import Types from "../components/common/Typography"
+import Box from "../components/common/Box"
+import SimpleTable from "../components/common/Table"
 
 export default () => {
   return (
-    <Layout>
-      <div className="styleguide-page">
-        <div>Style Guide</div>
-      </div>
-    </Layout>
+      <Layout>
+        <Box>
+          <Button color="primary" buttonName="Primary"/>
+        </Box>
+        <Box>
+          <Button color="secondary" buttonName="Secondary"/>
+        </Box>
+        <Types/>
+        <div className="styleguide-page">
+          <div>Style Guide</div>
+        </div>
+        <Box>
+          <SimpleTable/>
+        </Box>
+      </Layout>
+
   )
 }
