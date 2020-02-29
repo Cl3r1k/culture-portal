48974e3304860d71562b4b4217c901f77b9daeb7
Cl3r1k <Cl3r1k2@gmail.com>
2020-02-29 12:51:13 +0700

    refactor: add log in console

--- culture-portal/src/components/common/Language.js
+++ culture-portal/src/components/common/Language.js
@@ -6,7 +6,12 @@ const Language = () => {
   const { i18n } = useTranslation();
   const [currentLanguage, setLanguageState] = useState(i18n.language);
 
+  console.log('i18n.language', i18n.language);
+
   const setLanguage = (evt) => {
+    console.log('i18n.language', evt.target.tagName);
+    console.log('evt.target.tagName', evt.target.tagName);
+    console.log('evt.target.textContent', evt.target.textContent);
     if (evt.target.tagName === 'SPAN') {
       i18n.changeLanguage(evt.target.textContent);
       setLanguageState(evt.target.textContent);
@@ -14,7 +19,7 @@ const Language = () => {
   };
 
   return (
-    <div className="language" onClick={setLanguage}>
+    <div className="language" role="button" tabIndex="0" onClick={setLanguage}>
       {LANGUAGES.map((language) => (
         <span
           className={language === currentLanguage ? 'active' : undefined}
