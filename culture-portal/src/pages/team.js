e2a933701d78f00ab43cda6ac2efac1576a338c2
Cl3r1k <Cl3r1k2@gmail.com>
2020-02-29 13:11:46 +0700

    refactor: add log in console

--- culture-portal/src/pages/team.js
+++ culture-portal/src/pages/team.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../i18n/i18n';
 import Header from '../components/base/Header';
 import Layout from '../components/base/Layout';
 import DevelopersList from '../components/DevelopersList/DevelopersList';
